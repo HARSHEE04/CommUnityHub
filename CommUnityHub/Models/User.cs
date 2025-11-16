293be7a8bf66734fecda8885041c718b8d095ff1
Janki547 <janx547@gmail.com>
2025-11-15 20:06:48 -0500

    Added properties to user class and inherits IdentityUser to use ASP.Net Identity Framework

--- CommUnityHub/Models/User.cs
+++ CommUnityHub/Models/User.cs
@@ -1,6 +1,14 @@
-﻿namespace CommUnityHub.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CommUnityHub.Models
 {
-    public class User
+    public class User : IdentityUser
     {
+        public string FullName { get; set; }   
+
+        public bool IsVolunteer { get; set; } // Role-based 
+        public bool IsVerified { get; set; } // Verified by Admin
+
+        public bool IsSystemAdmin { get; set; } // Role-based
     }
 }
