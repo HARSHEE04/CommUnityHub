8216c148dc1b3f5c0da266e4e716bfa9aced7e0a
Kirill Mukhin <mukhin@shernet.sheridancollege.ca>
2025-11-19 01:16:22 -0500

    Added Offer Create and Index views

---
+++ CommUnityHub/Views/Offer/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CommUnityHub.Views.Offer
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
