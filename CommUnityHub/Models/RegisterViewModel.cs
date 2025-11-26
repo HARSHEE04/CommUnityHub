161df27fc0fa879d43f884529fb607c084f62b60
Janki547 <janx547@gmail.com>
2025-11-25 20:31:30 -0500

    Added RegisterViewModel and LoginViewModel for User registration form input and validating login input

---
+++ CommUnityHub/Models/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommUnityHub.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords must match")]
+        public string ConfirmPassword { get; set; }
+
+        public bool IsVolunteer { get; set; } // User selects volunteer role
+    }
+}
--- CommUnityHub/Models/User.cs
+++ CommUnityHub/Models/User.cs
@@ -5,10 +5,8 @@ namespace CommUnityHub.Models
     public class User : IdentityUser
     {
         public string FullName { get; set; }   
-
         public bool IsVolunteer { get; set; } // Role-based 
-        public bool IsVerified { get; set; } // Verified by Admin
-
+        public bool IsVerified { get; set; } // Volunteer Verified by Admin
         public bool IsSystemAdmin { get; set; } // Role-based
     }
 }
