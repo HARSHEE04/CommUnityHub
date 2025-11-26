e162032139c57b5f3acb585d126f08254591e364
Janki547 <janx547@gmail.com>
2025-11-25 20:45:30 -0500

    Created DbInitializer to seed a system admin

---
+++ CommUnityHub/Data/DbInitializer.cs
@@ -0,0 +1,29 @@
+﻿using CommUnityHub.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace CommUnityHub.Data
+{
+    public static class DBInitializer
+    {
+        public static void Initialize(UserIdentityDbContext context, UserManager<User> userManager)
+        {
+            context.Database.EnsureCreated();
+
+            // Seed system admin
+            string adminEmail = "admin@communityhub.com";
+            var admin = userManager.FindByEmailAsync(adminEmail).Result;
+            if (admin == null)
+            {
+                var sysAdmin = new User
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                    FullName = "System Admin",
+                    EmailConfirmed = true,
+                    IsSystemAdmin = true
+                };
+                userManager.CreateAsync(sysAdmin, "Admin123!").Wait();
+            }
+        }
+    }
+}
