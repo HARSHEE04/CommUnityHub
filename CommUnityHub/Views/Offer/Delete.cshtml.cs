906b904879fb95df7b0993bf3526fad591d2557e
Kirill Mukhin <mukhin@shernet.sheridancollege.ca>
2025-11-26 06:14:30 -0500

    Other part of Offer functiona;

---
+++ CommUnityHub/Views/Offer/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CommUnityHub.Views.Offer
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
